9be60c15f74cc65f06d273e87f97d461daae01b4
Elliptic Ellipsis <elliptic.ellipsis@gmail.com>
2022-02-07 15:53:35 +0000

    endian.h fix

--- lib/fairy/fairy.c
+++ lib/fairy/fairy.c
@@ -6,13 +6,30 @@
 #include "fairy.h"
 
 #include <assert.h>
-#include <endian.h>
 #include <stdarg.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+/* Different platforms put their endian files in different places. */
+#if defined(OS_MACOSX)
+  #include <machine/endian.h>
+#elif defined(OS_SOLARIS)
+  #include <sys/isa_defs.h>
+  #ifdef _LITTLE_ENDIAN
+    #define LITTLE_ENDIAN
+  #else
+    #define BIG_ENDIAN
+  #endif
+#elif defined(OS_FREEBSD) || defined(OS_OPENBSD) || defined(OS_NETBSD) ||\
+      defined(OS_DRAGONFLYBSD)
+  #include <sys/types.h>
+  #include <sys/endian.h>
+#else
+  #include <endian.h>
+#endif
 
 #include "vc_vector/vc_vector.h"
 
